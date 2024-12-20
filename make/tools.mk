96b62452c81374639f26fa2a96837dcad151998d
Marcin Benka <marcin.benka@gmail.com>
2024-12-20 19:38:01 +0100

    [obeny]: make: use host compiler by default

--- make/tools.mk
+++ make/tools.mk
@@ -30,7 +30,8 @@ CUT:=cut
 FGREP:=fgrep
 else
 # assume Linux
-ARM_SDK_PREFIX:=tools/linux/xpack-arm-none-eabi-gcc-10.3.1-2.3/bin/arm-none-eabi-
+#ARM_SDK_PREFIX:=tools/linux/xpack-arm-none-eabi-gcc-10.3.1-2.3/bin/arm-none-eabi-
+ARM_SDK_PREFIX:=arm-none-eabi-
 CP:=cp
 DSEP:=/
 NUL:=/dev/null
